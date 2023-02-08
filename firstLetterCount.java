151139429173293f3a4c3c88e289a35e7706cc5a
Aravind Sri Sai Kisore <62432516+aravindcmd@users.noreply.github.com>
2023-02-09 00:05:26 +0100

    Create firstLetterCount.java

---
+++ firstLetterCount.java
@@ -0,0 +1,85 @@
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class first {
+
+  public static class TokenizerMapper
+       extends Mapper<Object, Text, Text, IntWritable>{
+
+    private final static IntWritable one = new IntWritable(1);
+    private Text word = new Text();
+    
+    public void map(Object key, Text value, Context context
+                    ) throws IOException, InterruptedException {
+      String s = value.toString();
+      s = s.toUpperCase();
+      String f = new String();
+String[] arr = s.split(" ");
+ ArrayList<String> list = new ArrayList<String>();
+      for (String str : arr){
+          if (!str.equals("")){
+        list.add(str);
+    }
+      }
+      arr = list.toArray(new String[list.size()]);
+
+
+
+
+
+        for(int i = 0; i<arr.length;i++){
+            char c = arr[i].charAt(0);
+            f+=" "+c;
+            // String sub = arr[i].substring(1);
+        }
+
+      StringTokenizer itr = new StringTokenizer(f);
+      while (itr.hasMoreTokens()) {
+        word.set(itr.nextToken());
+        context.write(word, one);
+      }
+    }
+  }
+
+  public static class IntSumReducer
+       extends Reducer<Text,IntWritable,Text,IntWritable> {
+    private IntWritable result = new IntWritable();
+
+    public void reduce(Text key, Iterable<IntWritable> values,
+                       Context context
+                       ) throws IOException, InterruptedException {
+      int sum = 0;
+      for (IntWritable val : values) {
+        sum += val.get();
+      }
+      result.set(sum);
+      context.write(key, result);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    Configuration conf = new Configuration();
+    Job job = Job.getInstance(conf, "word count");
+    job.setJarByClass(first.class);
+    job.setMapperClass(TokenizerMapper.class);
+    job.setCombinerClass(IntSumReducer.class);
+    job.setReducerClass(IntSumReducer.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    FileInputFormat.addInputPath(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+    System.exit(job.waitForCompletion(true) ? 0 : 1);
+  }
+}
